a9bef21ef4c74afa5d793fda2877dd0287544917
Luke Frost <u1450920@unimail.hud.ac.uk>
2018-10-29 18:02:49 +0000

    added preg_match function to search for a valid URL.

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 <body>
 
 <?php
-
+$url = "";
 $urlError = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if (empty($_POST["url"])) {
-	$urlError = "URL needed";
+	$url = "";
 	} else {
 	$url = test_input($_POST["url"]);
-}
+	    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url)) {
+		$urlError = "URL isn't valid. Please enter a valid one";	
+  }
 }
 
 function test_input($data) {
@@ -19,11 +21,14 @@ function test_input($data) {
 	$data = htmlspecialchars($data);
 	return $data;	
 }
+
+}
 ?>
 
 <h2>Sputnik Digital Form Technical Test - Enter URL below</h2>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 URL of Website: <input type="text" name="url">
+<span class="error"><?php echo $urlError;?></span>
 <input type="submit">
 </form>
 
